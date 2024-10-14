0cba23faf8da66b7f2502b6bd2d6017116b22c4d
Alessandro <2ale2>
2024-10-14 02:02:35 +0200

    fixing setup script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,51 @@
 #!/bin/bash
 
-echo "Verifica aggiornamenti e installazione python3-venv..."
+echo "=== Verifica aggiornamenti e installazione python3-venv... ===
+
+"
 sudo apt update && sudo apt upgrade -y && sudo apt install -y python3-venv
 
-echo "Clonazione del progetto..."
+sleep 1
+
+echo "
+
+=== Clonazione del progetto ===
+
+"
 git clone https://github.com/2ale2/googleplaycheckertelegrambot_v2.git || {
   echo "Errore durante la clonazione del repository"
   exit 1
 }
 cd googleplaycheckertelegrambot_v2 || exit
+
+mkdir logs
+
+REPO_PATH="$(pwd)/googleplaycheckertelegrambot_v2"
+
+sleep 1
+
+echo "
+
+=== Aggiungendo il modulo 'modules' al PYTHONPATH ==="
+
+if ! grep -q "$REPO_PATH" ~/.bashrc; then
+  echo "export PYTHONPATH=\$PYTHONPATH:$REPO_PATH" >> ~/.bashrc
+  echo "
+
+=== Percorso aggiunto a PYTHONPATH ==="
+else
+    echo "
+
+=== i - Il percorso è già presente in PYTHONPATH ==="
+fi
+
+source "$HOME/.bashrc"
+
 sleep 2
 
-echo "Creazione dell'ambiente virtuale..."
+echo "
+
+=== Creazione dell'ambiente virtuale ==="
 python3 -m venv .venv || {
   echo "Errore durante la creazione dell'ambiente virtuale"
   exit 1
@@ -20,16 +54,24 @@ sleep 2
 
 source .venv/bin/activate
 
-echo "Installazione delle dipendenze..."
+sleep 1
+
+echo "
+
+=== Installazione delle dipendenze ==="
 pip install -r requirements.txt || {
-  echo "Errore durante l'installazione delle dipendenze"
+  echo "Errore durante l'installazione delle dipendenze
+
+  "
   exit 1
 }
 pip install "python-telegram-bot[callback-data]"
 pip install "python-telegram-bot[job-queue]"
 sleep 2
 
-echo "Decrittografia del file .env crittografato..."
+echo "
+
+=== Decrittografia del file .env crittografato... ==="
 success=0
 max_attempts=3
 attempts=0
@@ -37,15 +79,21 @@ sleep 2
 
 while [ $success -eq 0 ] && [ $attempts -lt $max_attempts ]; do
     ((attempts++))
-    echo "Tentativo $attempts di $max_attempts"
-    read -sp "Inserisci la password per decrittografare il file .env: " password
+    echo "
+
+> Tentativo $attempts di $max_attempts"
+    read -sp ">> Inserisci la password per decrittografare il file .env:" password
     echo
 
     if gpg --batch --passphrase "$password" -d .env.gpg > .env; then
         success=1
-        echo ".env decrittografato con successo."
+        echo "
+
+i - .env decrittografato con successo."
     else
-        echo "Password errata. Riprova."
+        echo "
+
+i - Password errata. Riprova."
     fi
 done
 
@@ -57,8 +105,22 @@ sleep 2
 
 
 if [ -f ".env" ]; then
-    echo "Il file .env è stato creato. Assicurati che le variabili nel file .env siano corrette!"
+    echo "
+
+=== Il file .env è stato creato ==="
 fi
 
-echo "Configurazione completata. Attiva l'ambiente virtuale con 'source venv/bin/activate' e avvia il bot con 'python3 main.py'"
+sleep 2
+
+echo "
+
+======== Configurazione completata ========
+
+Per avviare il bot, esegui questi comandi:
+1. cd googleplaycheckertelegrambot_v2
+2. source .venv/bin/activate
+3. python3 modules/main.py
+
+i - E' consigliabile aggiungere lo script come servizio per non doverlo avviare manualmente ogni volta
+"
 sleep 2
